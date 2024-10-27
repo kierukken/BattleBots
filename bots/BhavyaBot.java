50fa578d45b02eb7dae9921b810439b9241c4e9d
bg20061220 <1goelbha@hdsb.ca>
2024-10-27 19:42:11 -0400

    BOT DONE

--- bots/BhavyaBot.java
+++ bots/BhavyaBot.java
@@ -25,30 +25,50 @@ public class BhavyaBot extends Bot {
     private int count = 0 ; // Counter to implement the opening stratergy of the bot
     private boolean startShoot = true ; // Boolean To implement the opening stratergy of the bot
     private String moveToBot = "" ;  // String to store where to move to reach a bot
-    private boolean testing = true ; 
-    private BotInfo nearestBot ; 
-    private BotInfo dangerBot ; 
+    private BotInfo nearestBot ; // BotInfo object to store the closest Bot
+    private BotInfo dangerBot ;  
     private boolean danger = false  ; 
     private boolean shootDanger = false ; 
     private boolean bulletsShort = false ; 
     private String startDirection = "" ; 
-    
+    private boolean atEdge ;
+    private String edge ;  
+    private String shootDirection ;
+    private int moveCounter = 0 ; 
     @Override
     public void newRound() {
         count = 0 ; 
         startShoot = true ; 
         totalBulletsShot =0 ;
-
+        timeStamp = 0 ;
     }
 
     @Override
     public int getMove(BotInfo me, boolean shotOK, BotInfo[] liveBots, BotInfo[] deadBots, Bullet[] bullets) {
+        System.out.println(timeStamp);
+        // This function implements if the bot is at the edge
+        if(atEdge){
+            if(moveCounter == 5 ){
+                atEdge = false ; 
+            }
+            String lastShoot = shootDirection ; 
+            double meX = me.getX() ; 
+            if(edge.equals("AT TOP EDGE") && (meX < 5 || meX > 695) && (lastShoot.equals("SHOOT RIGHT") || lastShoot.equals("SHOOT LEFT")) ){
+                moveCounter++ ;
+                return BattleBotArena.DOWN ; 
+            }
+            if(edge.equals("AT BOTTOM EDGE") && (meX < 5 || meX > 695) && (lastShoot.equals("SHOOT RIGHT") || lastShoot.equals("SHOOT LEFT")) ){
+                moveCounter++ ;
+                return BattleBotArena.UP ; 
+            }
+        }
         /* CODE FOR NOT SHOOTING OUR TEAM BOTS NEXT THREE LINES */
+        
            List<BotInfo> oppBots = new ArrayList<>(Arrays.asList(liveBots));
            oppBots.removeIf(bot -> bot.getTeamName().equals("Warriors"));
            liveBots = oppBots.toArray(new BotInfo[0]);
            
-
+       // This block is used for making sure the bot never runs out of bullets
        if(totalBulletsShot > 25 && bulletsShort == false  ){
         shotOK = false ; 
         BotInfo closestDeadBot = bothelper.findClosest(me, deadBots);
@@ -60,6 +80,7 @@ public class BhavyaBot extends Bot {
             bulletsShort = true ; 
         }
        }
+       // This implements if there is a danger bot near me
        if(danger){
         danger = false ; 
         String direction  = getBotDirection(me, dangerBot) ; 
@@ -150,10 +171,14 @@ public class BhavyaBot extends Bot {
         BotInfo threatBot = isBotNearby(me, liveBots);
         if(threatBot != null){
             // Determining in which direction the danger Bot is 
-            String shootDirection = dangerBotShootDirection(me, threatBot);
+            shootDirection = dangerBotShootDirection(me, threatBot);
             if(shotOK){
                 totalBulletsShot++ ; 
                 bulletsShot++ ;
+                edge = shotAtEdge(me) ;
+                if (!edge.equals("NOT AT EDGE")) {
+                    atEdge = true ; 
+                }
                 return stringToCommand(shootDirection) ;
             }
         }
@@ -525,6 +550,11 @@ public class BhavyaBot extends Bot {
         }
         return null ; 
     }
+    /* 
+    *This function is used to check if the move that the bot is supposed to make safe or not
+    * @params - String moveDecision , BotInfo me , BotInfo livebots[]
+    * @returns - BotInfo object if there's a danger bot after the expected move is made 
+    */
     public BotInfo checkMoveSafety (String moveDecision , BotInfo me , BotInfo livebots[]){
         double newX = me.getX() ; 
         double newY = me.getY() ; 
@@ -543,6 +573,11 @@ public class BhavyaBot extends Bot {
         }
         return null ; 
     }
+    /* 
+    *This function is used to find the direction of a bot
+    * @params - BotInfo me , BotInfo targetBot
+    * @returns - String , direction of the bot
+    */
     public String getBotDirection(BotInfo me , BotInfo targetBot){
         if(Math.abs(targetBot.getX() - me.getX()) < 5 ){
             return (targetBot.getY() > me.getY()) ? "SHOOT DOWN" : "SHOOT UP" ;
@@ -552,17 +587,22 @@ public class BhavyaBot extends Bot {
         }
         return "DON'T SHOOT" ; 
     }
-    public boolean shotAtEdge(BotInfo me ){
-     int posY = Math.abs((int) me.getY() - (int) BattleBotArena.BOTTOM_EDGE) ; 
-     int posX = Math.abs((int) me.getX() - (int) BattleBotArena.RIGHT_EDGE) ;
-     if(posY > 695 || posY < 5 || posX > 1295 || posX < 5){ return true;}
-     else{return false; } 
-     if(posX > 1295){ return "MOVE RIGHT" ; }  
-     if(posX < 5){ return "MOVE LEFT" ; }
-     if(posY > 695){ return " MOVE DOWN" ; } 
-     if(posY < 5 ){ return " MOVE UP" ; }  
-     if(posX > 1295){ return "MOVE RIGHT" ; }  
-     if(posX < 5){ return "MOVE LEFT" ; }  
-         }
-         public String reactToEdge(me)
+    /* 
+     *This function is used to check if the bot has recently shot and if it is at an edge
+    * @params - BotInfo me 
+    * @returns - String , if it at any edge then that edge else return not at edge
+    */
+    public String shotAtEdge(BotInfo me){
+       double meX = me.getX() ; 
+       double meY = me.getY() ; 
+       double diffX = BattleBotArena.RIGHT_EDGE - meX;
+       double diffY = BattleBotArena.BOTTOM_EDGE - meY  ; 
+       if(diffX > 695){return "AT LEFT EDGE" ; }
+       else if(diffX < 5){return "AT RIGHT EDGE" ; }
+       else if(diffY > 1295){return "AT TOP EDGE";}
+       else if(diffY < 5 ){return "AT BOTTOM EDGE" ;}
+       else{return "NOT AT EDGE" ;}
+    }
+
+
 }
